ec3eb9303086bfc60cccebf32d33626456a74126
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-04-30 17:08:55 +0900

    create operation logs

--- react-apps/react-hooks-102/src/components/EventForm.js
+++ react-apps/react-hooks-102/src/components/EventForm.js
@@ -2,10 +2,13 @@ import React, { useContext, useState } from 'react';
 
 import { 
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS
   } from '../actions'
 
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
@@ -21,6 +24,12 @@ const EventForm = () => {
       body
     });
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    })
+
     setTitle('')
     setBody('')
   }
@@ -28,7 +37,15 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault();
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({ type: DELETE_ALL_EVENTS})
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS})
+
+      dispatch({ 
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601
+      })
+    }
   }
 
   const unCreatable = title === '' || body === '';
