9ae13fe005ee98edbe2a8503c597dfcd66372f82
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-04-30 16:30:17 +0900

    create react app by udemy

---
+++ react-apps/react-hooks-102/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import EventForm from './EventForm'
+import Events from './Events'
+import AppContext from '../contexts/AppContext'
+import reducer from '../reducers'
+
+const App = () => {
+  const initialState = {
+    events: []
+  }
+  const [ state, dispatch ] = useReducer(reducer, initialState)
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      <div className="container-fulid">
+        <EventForm />
+        <Events />
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App
