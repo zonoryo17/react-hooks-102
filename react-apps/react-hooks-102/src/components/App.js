98f39863f9fdfbbb4bc69d975ca40ff9858c39c1
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-05-01 00:38:28 +0900

    save state with lovalStotage

--- react-apps/react-hooks-102/src/components/App.js
+++ react-apps/react-hooks-102/src/components/App.js
@@ -1,24 +1,33 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import EventForm from './EventForm'
 import Events from './Events'
+import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY)
+  const initialState = appState ? JSON.parse(appState):{
     events: [],
     operationLogs: []
   }
   const [ state, dispatch ] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fulid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
